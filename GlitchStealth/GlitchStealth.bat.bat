ef5d73eba1ae0ab72a1cc8c2ca96f1bcbcc1a8b6
Stealth <69951037+Stealth-1337@users.noreply.github.com>
2020-08-27 17:11:34 +1200

    python

---
+++ GlitchStealth/GlitchStealth.bat.bat
@@ -0,0 +1,36 @@
+Title Made By Stealth Glitch
+
+
+
+
+@echo off
+color 1a
+pause
+cls
+echo Hello There! Prepare 4 the Screen of DETHH Made By Stealth!!
+pause
+cls
+mode 1100
+echo Made By Stealth!!
+echo Made By Stealth!!
+echo Made By Stealth!!
+ 
+:A
+echo %random% %random% %random% %random%   %random% %random% %random% %random%   %random% %random% %random% %random% %random%   %random% %random% %random% %random% %random%   %random% %random% %random% %random%   %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random%
+echo %random% %random% %random% %random%   %random% %random% %random% %random%   %random% %random% %random% %random% %random%   %random% %random% %random% %random% %random%   %random% %random% %random% %random%   %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random%
+color 0a
+color 01
+color 1a
+color 0b
+color 02
+color 32
+color 20
+color 4c
+color 5b
+color 3d
+color d1
+color 10
+color 0b
+color b3
+color 52
+goto :A
